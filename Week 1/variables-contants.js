68640800c14bed0ffa98897c4ee7482aba302325
kthrnbeh <kthrnbeh@yahoo.com>
2026-09-17 12:29:34 -0600

    updates add

--- Week 1/variables-contants.js
+++ Week 1/variables-contants.js
@@ -2,3 +2,13 @@ const PI =3.14;
 let radius = 3;
 let area = PI * radius * radius;
 console.log (area);
+
+radius = 20;
+area = PI * radius * radius;
+console.log (area);
+
+//type cohersion
+const one = 1;
+const two = '2';
+let result = one *  two;
+console.log(result);
